79556d3abb113bee962cb9326a7d43f193bf9a1f
Evan Siegel <siegel.evan@gmail.com>
2023-06-03 10:11:43 -0400

    Adds Command Handling
    All of the commands we put into the commands directory (and add to the
    collection in commands/index) will be handled by the application.

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 import { Client, Events, GatewayIntentBits } from "discord.js";
 import dotenv from "dotenv";
 import express from "express";
+import commands from "./src/commands";
 
 dotenv.config();
 
@@ -22,6 +23,40 @@ const client = new Client({
 
 client.once(Events.ClientReady, ({ user }) => {
   console.log(`🤖Ready. Logged in as ${user.tag}.`);
+});
+
+client.on(Events.InteractionCreate, (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(
+      `🚨 Could not find command called ${interaction.commandName}.`
+    );
+    return;
+  }
+
+  return command
+    .execute(interaction)
+    .catch((reason: unknown): Promise<unknown> => {
+      console.error(`🚨 ${reason}`);
+
+      if (interaction.replied || interaction.deferred) {
+        return interaction.followUp({
+          content: "Something went wrong!",
+          ephemeral: true,
+        });
+      } else {
+        return interaction.reply({
+          content: "Something went wrong!",
+          ephemeral: true,
+        });
+      }
+    })
+    .then(() => {
+      console.log(`🤖 ${interaction.commandName} handled.`);
+    });
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
