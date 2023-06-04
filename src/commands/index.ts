5a1833bb21d8bebacbd73ede9983da69768cf14f
Evan Siegel <siegel.evan@gmail.com>
2023-06-04 10:42:24 -0400

    Removes /ping

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,9 +1,7 @@
 import { Collection } from "discord.js";
 import Command from "../Command";
-import ping from "./ping";
 
 const commands = new Collection<string, Command>();
 
-commands.set(ping.data.name, ping);
 
 export default commands;
