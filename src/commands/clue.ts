e4ccb39bb4bdadd90c393ad65fd764cc4220fbef
Evan Siegel <siegel.evan@gmail.com>
2023-06-05 07:40:44 -0400

    Introduces the /clue Command
    This command returns a random clue given a ClueId ("1a", etc.). It also
    has some protections against bad or incomplete data.

---
+++ src/commands/clue.ts
@@ -0,0 +1,56 @@
+import { SlashCommandBuilder } from "discord.js";
+import { pipe } from "fp-ts/function";
+import { isLeft, chain, fromNullable } from "fp-ts/Either";
+import { isNone } from "fp-ts/Option";
+import Command from "../Command";
+import { fromString, toString } from "../ClueId";
+import { findRandomById } from "../Clue";
+
+const clue: Command = {
+  data: new SlashCommandBuilder()
+    .setName("clue")
+    .setDescription("Get a clue by id.")
+    .addStringOption((option) =>
+      option
+        .setName("clue_id")
+        .setDescription(
+          "A number and 'A' for across or 'D' for down; e.g. '5D'."
+        )
+        .setRequired(true)
+    ),
+
+  execute: (interaction) => {
+    const clueId = pipe(
+      interaction.options.getString("clue_id"),
+      fromNullable(new Error("Missing required option: clue_id")),
+      chain(fromString)
+    );
+
+    if (isLeft(clueId)) {
+      return interaction.reply({
+        content: clueId.left.message,
+        ephemeral: true,
+      });
+    }
+
+    return findRandomById(clueId.right)
+      .then((clue) => {
+        if (isNone(clue)) {
+          return interaction.reply({
+            content: `Cannot find clue with id ${toString(clueId.right)}.`,
+            ephemeral: true,
+          });
+        }
+
+        return interaction.reply(clue.value.clue);
+      })
+      .catch((reason) => {
+        return interaction.reply({
+          content: `${reason}`,
+          ephemeral: true,
+        });
+      });
+  },
+};
+
+export default clue;
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,7 +1,9 @@
 import { Collection } from "discord.js";
 import Command from "../Command";
+import clue from "./clue";
 
 const commands = new Collection<string, Command>();
 
+commands.set(clue.data.name, clue);
 
 export default commands;
